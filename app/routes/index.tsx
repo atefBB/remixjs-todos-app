29bc664b33e47b3353565aea8b01bca61ad59e58
Atef Ben Ali <atef.bettaib@gmail.com>
2022-01-23 15:23:17 +0100

    feat: prepare `todos`

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,100 +1,57 @@
 import type { MetaFunction, LoaderFunction } from "remix";
-import { useLoaderData, json, Link } from "remix";
+import { useLoaderData, json } from "remix";
 
 type IndexData = {
-  resources: Array<{ name: string; url: string }>;
-  demos: Array<{ name: string; to: string }>;
+  todos: Array<{ task: string; status: "done" | "todo" }>;
 };
 
 // Loaders provide data to components and are only ever called on the server, so
 // you can connect to a database or run any server side code you want right next
 // to the component that renders it.
 // https://remix.run/api/conventions#loader
-export let loader: LoaderFunction = () => {
-  let data: IndexData = {
-    resources: [
+export const loader: LoaderFunction = () => {
+  const data: IndexData = {
+    todos: [
       {
-        name: "Remix Docs",
-        url: "https://remix.run/docs"
+        task: "Read Clean Code Book",
+        status: "done",
       },
       {
-        name: "React Router Docs",
-        url: "https://reactrouter.com/docs"
+        task: "Lean Remix",
+        status: "todo",
       },
       {
-        name: "Remix Discord",
-        url: "https://discord.gg/VBePs6d"
-      }
+        task: "Lean Typescript",
+        status: "done",
+      },
     ],
-    demos: [
-      {
-        to: "demos/actions",
-        name: "Actions"
-      },
-      {
-        to: "demos/about",
-        name: "Nested Routes, CSS loading/unloading"
-      },
-      {
-        to: "demos/params",
-        name: "URL Params and Error Boundaries"
-      }
-    ]
   };
 
-  // https://remix.run/api/remix#json
   return json(data);
 };
 
 // https://remix.run/api/conventions#meta
-export let meta: MetaFunction = () => {
+export const meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
-    description: "Welcome to remix!"
+    description: "Welcome to remix!",
   };
 };
 
 // https://remix.run/guides/routing#index-routes
 export default function Index() {
-  let data = useLoaderData<IndexData>();
+  const data = useLoaderData();
 
   return (
-    <div className="remix__page">
-      <main>
-        <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
-          Feel free to take a look around the code to see how Remix does things,
-          it might be a bit different than what you’re used to. When you're
-          ready to dive deeper, we've got plenty of resources to get you
-          up-and-running quickly.
-        </p>
-        <p>
-          Check out all the demos in this starter, and then just delete the{" "}
-          <code>app/routes/demos</code> and <code>app/styles/demos</code>{" "}
-          folders when you're ready to turn this into your next project.
-        </p>
-      </main>
-      <aside>
-        <h2>Demos In This App</h2>
-        <ul>
-          {data.demos.map(demo => (
-            <li key={demo.to} className="remix__page__resource">
-              <Link to={demo.to} prefetch="intent">
-                {demo.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <h2>Resources</h2>
-        <ul>
-          {data.resources.map(resource => (
-            <li key={resource.url} className="remix__page__resource">
-              <a href={resource.url}>{resource.name}</a>
-            </li>
-          ))}
-        </ul>
-      </aside>
-    </div>
+    <section className="todos__clz">
+      {data.todos.map(({ task, status }, index) => (
+        <div key={index} className="todo-container__clz">
+          <input type="checkbox" checked={status === "done" ? "checked" : ""} />
+          <article key={index} className="todo__clz">
+            {task}
+          </article>
+        </div>
+      ))}
+    </section>
   );
 }
